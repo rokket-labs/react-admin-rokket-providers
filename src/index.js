ac8b1cc674ed20d01b28dd76ec1bd0760bbdae40
eliotarellano <eliot.arellano.94@gmail.com>
2020-01-29 15:57:26 -0300

    building data/auth providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import DataProvider from './Providers/dataProvider'
+import AuthProvider from './Providers/authProvider'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export { DataProvider, AuthProvider }
